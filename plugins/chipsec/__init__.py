48c9ee36bb19279aa749d35b540119f84c5fa1dd
Richard Hughes <richard@hughsie.com>
2019-06-02 08:06:26 +0100

    chipsec: Fix extacting shards from Dell firmware
    Dell embed a PFS structure in a Zlib-compressed section of the main EFI binary
    included as the content in the cab archive.
    
    If there are no shards reported from 'chipsec uefi decode' try to find a
    suitable size Zlib section nd then try again with that.

--- plugins/chipsec/__init__.py
+++ plugins/chipsec/__init__.py
@@ -11,74 +11,41 @@ import glob
 import os
 import re
 import subprocess
+import zlib
 
 from app import db
 from app.pluginloader import PluginBase, PluginError, PluginSettingBool, PluginSettingText
 from app.models import Test, ComponentShard
 
-def _add_component_shards(self, md, files):
+def _find_dell_pfs(blob):
+    offset = 0
 
-    # remove any old shards we added
-    for shard in md.shards:
-        if shard.plugin_id == self.id:
-            db.session.delete(shard)
-
-    # should we write to disk for later processing
-    save = self.get_setting_bool('chipsec_write_shards')
-
-    # parse each EFI binary as a shard
-    for fn in files:
-        sections = fn.rsplit('/')
-        name = sections[-1].split('.')[0]
-        guid = None
-        for section in reversed(sections[:-1]):
-            if section.find('GUID_DEFINED') != -1:
-                continue
-            if section.find('COMPRESSION') != -1:
-                continue
-            guid = section.split('_')[1].split('.')[0].lower()
+    while 1:
+        # find Zlib header for default compression
+        offset_next = blob.find(b'\x78\x9C')
+        if offset_next == -1:
             break
-        if not guid:
-            continue
 
-        shard = ComponentShard(component_id=md.component_id, plugin_id=self.id)
-        shard.ensure_info(guid, name)
-        with open(fn, 'rb') as f:
-            shard.set_blob(f.read(), save=save)
+        # decompress the buffer, which also checks if it's really Zlib or just
+        # two random bytes that happen to match for no good reason
+        try:
+            blob_decompressed = zlib.decompress(blob[offset_next:])
+        except zlib.error as _:
+            offset_next += 2
+        else:
+            # only include blobs greater than 512kB in size
+            if len(blob_decompressed) > 0x80000:
+                return blob_decompressed
+            offset_next += 2 + len(blob_decompressed)
 
-        # add shard to component
-        md.shards.append(shard)
+        blob = blob[offset_next:]
+        offset += offset_next
 
-def _run_chipsec_on_blob(self, test, md):
-
-    # write firmware to temp file
-    cwd = tempfile.TemporaryDirectory(prefix='lvfs')
-    src = tempfile.NamedTemporaryFile(mode='wb',
-                                      prefix='lvfs_',
-                                      suffix=".bin",
-                                      dir=cwd.name,
-                                      delete=False)
-    src.write(md.blob)
-    src.flush()
-
-    # run chipsec
-    cmd = self.get_setting('chipsec_binary', required=True)
-    argv = [cmd, '--no_driver', 'uefi', 'decode', src.name]
-    ps = subprocess.Popen(argv, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd.name)
-    if ps.wait() != 0:
-        raise PluginError('Failed to decode file: %s' % ps.stderr.read())
-
-    # look for shards
-    outdir = src.name + '.dir'
-    files = glob.glob(outdir + '/FV/**/*.efi', recursive=True)
-    if not files:
-        test.add_pass('No firmware volumes found in {}'.format(md.filename_contents))
-        return
-    _add_component_shards(self, md, files)
+    return None
 
 class Plugin(PluginBase):
-    def __init__(self):
-        PluginBase.__init__(self)
+    def __init__(self, plugin_id=None):
+        PluginBase.__init__(self, plugin_id)
 
     def name(self):
         return 'CHIPSEC'
@@ -92,6 +59,87 @@ class Plugin(PluginBase):
         s.append(PluginSettingBool('chipsec_write_shards', 'Write shards to disk', True))
         s.append(PluginSettingText('chipsec_binary', 'CHIPSEC executable', 'chipsec_util'))
         return s
+
+    def _convert_files_to_shards(self, files):
+
+        # should we write to disk for later processing
+        save = self.get_setting_bool('chipsec_write_shards')
+
+        # parse each EFI binary as a shard
+        shards = []
+        for fn in files:
+            sections = fn.rsplit('/')
+            name = sections[-1].split('.')[0]
+            guid = None
+            for section in reversed(sections[:-1]):
+                if section.find('GUID_DEFINED') != -1:
+                    continue
+                if section.find('COMPRESSION') != -1:
+                    continue
+                guid = section.split('_')[1].split('.')[0].lower()
+                break
+            if not guid:
+                continue
+
+            shard = ComponentShard(plugin_id=self.id)
+            shard.ensure_info(guid, name)
+            with open(fn, 'rb') as f:
+                shard.set_blob(f.read(), save=save)
+            shards.append(shard)
+        return shards
+
+    def _get_shards_for_blob(self, blob):
+
+        # write blob to temp file
+        cwd = tempfile.TemporaryDirectory(prefix='lvfs')
+        src = tempfile.NamedTemporaryFile(mode='wb',
+                                          prefix='lvfs_',
+                                          suffix=".bin",
+                                          dir=cwd.name,
+                                          delete=False)
+        src.write(blob)
+        src.flush()
+
+        # run chipsec
+        cmd = self.get_setting('chipsec_binary', required=True)
+        argv = [cmd, '--no_driver', 'uefi', 'decode', src.name]
+        ps = subprocess.Popen(argv, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd.name)
+        if ps.wait() != 0:
+            raise PluginError('Failed to decode file: %s' % ps.stderr.read())
+
+        # look for shards
+        outdir = src.name + '.dir'
+        files = glob.glob(outdir + '/FV/**/*.efi', recursive=True)
+        return self._convert_files_to_shards(files)
+
+    def _run_chipsec_on_md(self, test, md):
+
+        # remove any old shards we added
+        for shard in md.shards:
+            if shard.plugin_id == self.id:
+                db.session.delete(shard)
+
+        # try first with the plain blob (possibly with a capsule header) and
+        # then look for a Zlib section (with an optional PFS-prefixed) blob
+        shards = self._get_shards_for_blob(md.blob)
+        if not shards:
+            blob = _find_dell_pfs(md.blob)
+            if blob:
+                if blob.startswith(b'PFS.HDR.'):
+                    test.add_pass('Found PFS in Zlib compressed blob')
+                    # don't parse the PFS as chipsec just does blob.find('_FVH')
+                    # anyway: https://github.com/LongSoft/PFSExtractor
+                else:
+                    test.add_pass('Found Zlib compressed blob')
+                shards = self._get_shards_for_blob(blob)
+        if not shards:
+            test.add_pass('No firmware volumes found in {}'.format(md.filename_contents))
+            return
+
+        # add shard to component
+        for shard in shards:
+            shard.component_id = md.component_id
+            md.shards.append(shard)
 
     def _require_test_for_md(self, md):
         if not md.protocol:
@@ -121,5 +169,26 @@ class Plugin(PluginBase):
                 continue
             if not md.blob:
                 continue
-            _run_chipsec_on_blob(self, test, md)
+            self._run_chipsec_on_md(test, md)
         db.session.commit()
+
+# run with PYTHONPATH=. ./.env3/bin/python3 plugins/chipsec/__init__.py ./firmware.bin
+if __name__ == '__main__':
+    import sys
+    from app.models import Firmware, Component, Protocol
+
+    for _argv in sys.argv[1:]:
+        print('Processing', _argv)
+        plugin = Plugin('chipsec')
+        _test = Test(plugin.id)
+        _fw = Firmware()
+        _md = Component()
+        _md.filename_contents = 'filename.bin'
+        _md.protocol = Protocol('org.uefi.capsule')
+        with open(_argv, 'rb') as _f:
+            _md.blob = _f.read()
+        _fw.mds.append(_md)
+        plugin.run_test_on_fw(_test, _fw)
+        for attribute in _test.attributes:
+            print(attribute)
+        print(_md.shards)
