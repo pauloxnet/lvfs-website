cd31e48b31fc1e34de40b18d079da26f9da6210d
Richard Hughes <richard@hughsie.com>
2019-06-03 15:03:08 +0100

    Add a simple script to report test failures
    The CSV output can be used for CVE generation, for example...

---
+++ report.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015-2018 Richard Hughes <richard@hughsie.com>
+# Licensed under the GNU General Public License Version 2
+#
+# pylint: disable=singleton-comparison,wrong-import-position
+
+import sys
+import csv
+
+import app as application   #lgtm [py/import-and-import-from]
+from app import db
+
+from app.models import Firmware
+
+# make compatible with Flask
+app = application.app
+
+def _write_report_for_plugin_id(writer, plugin_id):
+
+    fws = db.session.query(Firmware).all()
+    for fw in fws:
+        if fw.is_deleted:
+            continue
+        if not fw.remote.is_public:
+            continue
+        data = {'plugin': plugin_id,
+                'vendor': fw.vendor.group_id,
+                'model': fw.md_prio.name,
+                'version': fw.md_prio.version_display}
+        for test in fw.tests:
+            if test.success:
+                continue
+            if test.plugin_id != plugin_id:
+                continue
+            for attr in test.attributes:
+                if attr.success:
+                    continue
+                data['shard'] = attr.title
+                data['message'] = attr.message
+                writer.writerow(data)
+
+def _run_report_for_plugin_ids(filename, plugin_ids):
+    with open(filename, 'w', newline='') as csvfile:
+        fieldnames = ['plugin', 'vendor', 'model', 'version', 'shard', 'message']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for plugin_id in plugin_ids:
+            _write_report_for_plugin_id(writer, plugin_id)
+
+if __name__ == '__main__':
+
+    # need at least one plugin ID
+    if len(sys.argv) < 2:
+        print('Usage: %s [pecheck] [blocklist]' % sys.argv[0])
+        sys.exit(1)
+
+    try:
+        with app.test_request_context():
+            _run_report_for_plugin_ids('report.csv', sys.argv[1:])
+    except NotImplementedError as e:
+        print(str(e))
+        sys.exit(1)
