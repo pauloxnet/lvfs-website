c0de4e912cf10405c95aa1e76a49565375f3c7d0
Richard Hughes <richard@hughsie.com>
2019-11-08 12:23:00 +0000

    Use PSPTool to parse the AMD PSP section
    This is very similar to how the IntelME parser works.

---
+++ plugins/amdpsp/__init__.py
@@ -0,0 +1,122 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019 Richard Hughes <richard@hughsie.com>
+#
+# SPDX-License-Identifier: GPL-2.0+
+#
+# pylint: disable=no-self-use,no-member,too-few-public-methods
+
+import os
+import struct
+import uuid
+
+from psptool import PSPTool
+from psptool.entry import PubkeyEntry, HeaderEntry
+from psptool.blob import Blob
+
+from lvfs import db
+from lvfs.pluginloader import PluginBase, PluginError, PluginSettingBool
+from lvfs.models import Test, ComponentShard
+
+def _mkguid(value):
+    if not value:
+        return None
+    return str(uuid.uuid5(uuid.NAMESPACE_DNS, value))
+
+def _get_readable_type(entry):
+    if not entry.type in entry.DIRECTORY_ENTRY_TYPES:
+        return hex(entry.type)
+    return entry.DIRECTORY_ENTRY_TYPES[entry.type].replace('!', '')
+
+def _run_psptool_on_blob(self, test, md):
+
+    # remove any old shards we added
+    for shard in md.shards:
+        if shard.plugin_id == self.id:
+            db.session.delete(shard)
+    db.session.commit()
+
+    # parse firmware
+    try:
+        psp = PSPTool(md.blob, verbose=True)
+        for directory in psp.blob.directories:
+            for entry in directory.entries:
+                if isinstance(entry, HeaderEntry):
+                    blob = entry.get_decompressed()
+                    appstream_id = 'com.amd.PSP.HeaderEntry.{}'.\
+                                        format(_get_readable_type(entry))
+                elif isinstance(entry, PubkeyEntry):
+                    blob = entry.get_pem_encoded()
+                    appstream_id = 'com.amd.PSP.Entry.{}'.\
+                                        format(_get_readable_type(entry))
+                else:
+                    blob = entry.get_bytes()
+                    appstream_id = 'com.amd.PSP.{}'.\
+                                        format(_get_readable_type(entry))
+
+                # add shard to component
+                shard = ComponentShard(component_id=md.component_id,
+                                       plugin_id=self.id,
+                                       guid=_mkguid(hex(entry.type)),
+                                       name=appstream_id)
+                shard.set_blob(blob, checksums='SHA256')
+                md.shards.append(shard)
+        test.add_pass('Found {} directories'.format(len(psp.blob.directories)))
+    except Blob.NoFirmwareEntryTableError as _:
+        pass
+
+class Plugin(PluginBase):
+    def __init__(self, plugin_id=None):
+        PluginBase.__init__(self, plugin_id)
+
+    def name(self):
+        return 'AMD PSP'
+
+    def summary(self):
+        return 'Analyse modules in AMD PSP firmware'
+
+    def settings(self):
+        s = []
+        s.append(PluginSettingBool('amdpsp_enabled', 'Enabled', True))
+        return s
+
+    def _require_test_for_md(self, md):
+
+        # match on protocol
+        if not md.protocol:
+            # only until required
+            return True
+        if md.protocol.value != 'org.uefi.capsule':
+            return False
+
+        # match on category
+        if not md.category:
+            # only until required
+            return True
+        return md.category.matches(['X-System', 'X-PlatformSecurityProcessor'])
+
+    def _require_test_for_fw(self, fw):
+        for md in fw.mds:
+            if self._require_test_for_md(md):
+                return True
+        return False
+
+    def ensure_test_for_fw(self, fw):
+
+        # add if not already exists
+        if self._require_test_for_fw(fw):
+            test = fw.find_test_by_plugin_id(self.id)
+            if not test:
+                test = Test(self.id, waivable=True)
+                fw.tests.append(test)
+
+    def run_test_on_fw(self, test, fw):
+
+        # run psptool on the capsule data
+        for md in fw.mds:
+            if not md.blob:
+                continue
+            if self._require_test_for_md(md):
+                _run_psptool_on_blob(self, test, md)
+        db.session.commit()
---
+++ plugins/amdpsp/debug.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019 Richard Hughes <richard@hughsie.com>
+#
+# SPDX-License-Identifier: GPL-2.0+
+#
+# pylint: disable=protected-access,wrong-import-position
+
+import os
+import sys
+
+# allows us to run this from the project root
+sys.path.append(os.path.realpath('.'))
+
+from lvfs.models import Test, Firmware, Component, Protocol, Category
+from plugins.amdpsp import Plugin
+
+if __name__ == '__main__':
+    for _argv in sys.argv[1:]:
+        print('Processing', _argv)
+        plugin = Plugin('amdpsp')
+        _test = Test(plugin.id)
+        _fw = Firmware()
+        _md = Component()
+        _md.component_id = 999999
+        _md.category = Category('X-PlatformSecurityProcessor')
+        _md.filename_contents = 'filename.bin'
+        _md.protocol = Protocol('org.uefi.capsule')
+        with open(_argv, 'rb') as _f:
+            _md.blob = _f.read()
+        _fw.mds.append(_md)
+        plugin.run_test_on_fw(_test, _fw)
+        for attribute in _test.attributes:
+            print(attribute)
+        for shard in _md.shards:
+            print(shard.guid, shard.name, shard.checksum, len(shard.blob))
